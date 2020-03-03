9fe9238c0dd08db601d240178be056be148d46c3
V1ncentLiu <vincentliu9816@gmail.com>
2020-03-03 11:30:23 -0500

    First update, regex in script is in.

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+    Subject: COMP-125
+    File Name: Assignment 4 
+    Author: Peitong Liu 
+    ID: 300913117
+*/
+var regexForPhone = /^[0-9]{10}$/;
+var regexCdnZip = /^([A-Z]\d[A-Z]\s?\d[A-Z]\d)$/;
+var regexUsaZip = /^[0-9]{5}(?:-[0-9]{4})?$/;
+var regexEmail = /^[_a-zA-Z0-9]+(\.[_a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*(\.[a-z]{2,6})$/;
+var regexPwd = /^[a-zA-Z0-9\\\.]{8,12}$/;
