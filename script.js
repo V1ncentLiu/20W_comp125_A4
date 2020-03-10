77fb00805437985aa9fcbd403607a69b09b625da
V1ncentLiu <vincentliu9816@gmail.com>
2020-03-10 12:24:57 -0400

    20200310 #2

--- script.js
+++ script.js
@@ -13,32 +13,39 @@ var regexCdnZip = /^([A-Z]\d[A-Z]\s?\d[A-Z]\d)$/;
 var regexUsaZip = /^[0-9]{5}(?:-[0-9]{4})?$/;
 var regexEmail = /^[_a-zA-Z0-9]+(\.[_a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*(\.[a-z]{2,6})$/;
 var regexPwd = /^[a-zA-Z0-9]{8,12}$/;
-var regexName = /^[A-Z][a-z]{3,15}$/;
-var profile = {};
+var regexName = /^[A-Z]*[a-z]{3,15}$/;
 
-function checkNames(){
-    var fnInput = document.getElementById("inputFN");
-    var lnInput = document.getElementById("inputLN");
-    var fnerror = document.getElementById("fnError");
-    var lnerror = document.getElementById("lnError");
+function checkFName(){
+    var fnInput = document.getElementById("inputFN");    
+    var fnLabel = document.getElementById("lblFN");
+	var fnCell = document.getElementById("cellFN");
     try{
-        if (fnInput.value.length < 3){
-            throw "Invalid User Name!";
+        if (regexName.test(fnInput) === false){
+            throw "Invalid first name!";
         }
-        fnInput.style.background = "";
-        lnInput.style.background = "";
-        fnerror.style.display = "none";
-        lnerror.style.display = "none";
-        fnerror.innerHTML = "";
-        lnerror.innerHTML = "";
+        fnCell.style.background = "";                
+        fnLabel.innerHTML = "First Name";        
     }
     catch(msg){
-        fnerror.style.display = "block";
-        fnerror.innerHTML = msg;
-        fnInput.style.background = "rgb(255,233,233)";
-        lnerror.style.display = "block";
-        lnerror.innerHTML = msg;
-        lnInput.style.background = "rgb(255,233,233)";
+        fnLabel.innerHTML = msg;
+        fnCell.style.background = "rgb(255,233,233)";
+    }
+}
+
+function checkLName(){
+    var lnInput = document.getElementById("inputLN");    
+    var lnLabel = document.getElementById("lblLN");
+	var lnCell = document.getElementById("cellLN");
+    try{
+        if (regexName.test(lnInput) === false){
+            throw "Invalid last name!";
+        }
+        lnCell.style.background = "";                
+        lnLabel.innerHTML = "Last Name";        
+    }
+    catch(msg){
+        lnLabel.innerHTML = msg;
+        lnCell.style.background = "rgb(255,233,233)";
     }
 }
 
@@ -55,24 +62,19 @@ function checkPhone(){
 }
 
 function createEventListeners(){
-    var firstname = document.getElementById("inputFn");
+    var firstname = document.getElementById("inputFN");
     var lastname = document.getElementById("inputLN");
     var email = document.getElementById("inputEM");
     if (firstname.addEventListener){
-        firstname.addEventListener("change", checkNames, false);
+        firstname.addEventListener("change", checkFName, false);
         lastname.addEventListener("change", checkNames, false);
         email.addEventListener("change", checkEmail, false);
     }else if(firstname.attachEvent){
-        firstname.attachEvent("onchange", checkNames);
+        firstname.attachEvent("onchange", checkFName);
         lastname.attachEvent("onchange", checkNames);
         email.attachEvenet("onchange", checkEmail);
     }
 }
-
-
-
-
-
 
 if (window.addEventListener) {
    window.addEventListener("load", createEventListeners, false);
